9869f196489d2e057d1c385d52cfdc17b20c1e54
Rustik123569 <rustam123569@mail.ru>
2024-04-26 18:46:39 +0500

    Add ConcreteServiceCheckbox

--- src/CleaningServiceCalculator.java
+++ src/CleaningServiceCalculator.java
@@ -29,6 +29,26 @@ public class CleaningServiceCalculator extends JFrame {
     }
 
     private class ConcreteServiceCheckBox extends ServiceCheckBox {
+        public ConcreteServiceCheckBox(String text, double cost) {
+            super(text, cost);
+        }
+
+        @Override
+        public void updateCost(String text, double cost) {
+            if (text.equals("Чистка дивана 1000р/шт")) {
+                sofaCost = cost;
+            } else if (text.equals("Мойка посуды 400р")) {
+                carpetsCost = cost;
+            } else if (text.equals("Мойка окон 300р/шт")) {
+                windowsCost = cost;
+            } else if (text.equals("Мойка кухни 4000р")) {
+                kitchen = cost;
+            } else if (text.equals("Мойка стен 500р/стена")) {
+                wall = cost;
+            } else if (text.equals("Сложные изделия 300р/шт")) {
+                complexProducts = cost;
+            }
+        }
 
     }
 
